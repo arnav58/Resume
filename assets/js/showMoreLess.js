bcb68f481b6b18cee64354069e9d0264ac2902ff
Arnav <Arnav>
2016-07-09 14:54:14 +0530

    Changed design. Added Show Desc/See Less button.

---
+++ assets/js/showMoreLess.js
@@ -0,0 +1,90 @@
+function toggleSeeMore1() {
+    if(document.getElementById("textarea1").style.display == 'none') {
+        document.getElementById("textarea1").style.display = 'block';
+        document.getElementById("seeMore1").innerHTML = 'See less';
+        document.getElementById("seeMore1").style.color = "red";
+    }
+    else if (document.getElementById("textarea1").style.display != 'none') {
+        document.getElementById("textarea1").style.display = 'none';
+        document.getElementById("seeMore1").innerHTML = 'Description';
+        document.getElementById("seeMore1").style.color = "blue";        
+    }
+}
+
+function toggleSeeMore2() {
+    if(document.getElementById("textarea2").style.display == 'none') {
+        document.getElementById("textarea2").style.display = 'block';
+        document.getElementById("seeMore2").innerHTML = 'See less';
+        document.getElementById("seeMore2").style.color = "red";
+    }
+    else if (document.getElementById("textarea2").style.display != 'none') {
+        document.getElementById("textarea2").style.display = 'none';
+        document.getElementById("seeMore2").innerHTML = 'Description';
+        document.getElementById("seeMore2").style.color = "blue";        
+    }
+}
+
+function toggleSeeMore3() {
+    if(document.getElementById("textarea3").style.display == 'none') {
+        document.getElementById("textarea3").style.display = 'block';
+        document.getElementById("seeMore3").innerHTML = 'See less';
+        document.getElementById("seeMore3").style.color = "red";
+    }
+    else if (document.getElementById("textarea3").style.display != 'none') {
+        document.getElementById("textarea3").style.display = 'none';
+        document.getElementById("seeMore3").innerHTML = 'Description';
+        document.getElementById("seeMore3").style.color = "blue";        
+    }
+}
+
+function toggleSeeMore4() {
+    if(document.getElementById("textarea4").style.display == 'none') {
+        document.getElementById("textarea4").style.display = 'block';
+        document.getElementById("seeMore4").innerHTML = 'See less';
+        document.getElementById("seeMore4").style.color = "red";
+    }
+    else if (document.getElementById("textarea4").style.display != 'none') {
+        document.getElementById("textarea4").style.display = 'none';
+        document.getElementById("seeMore4").innerHTML = 'Description';
+        document.getElementById("seeMore4").style.color = "blue";        
+    }
+}
+
+function toggleSeeMore5() {
+    if(document.getElementById("textarea5").style.display == 'none') {
+        document.getElementById("textarea5").style.display = 'block';
+        document.getElementById("seeMore5").innerHTML = 'See less';
+        document.getElementById("seeMore5").style.color = "red";
+    }
+    else if (document.getElementById("textarea5").style.display != 'none') {
+        document.getElementById("textarea5").style.display = 'none';
+        document.getElementById("seeMore5").innerHTML = 'Description';
+        document.getElementById("seeMore5").style.color = "blue";        
+    }
+}
+
+function toggleSeeMore6() {
+    if(document.getElementById("textarea6").style.display == 'none') {
+        document.getElementById("textarea6").style.display = 'block';
+        document.getElementById("seeMore6").innerHTML = 'See less';
+        document.getElementById("seeMore6").style.color = "red";
+    }
+    else if (document.getElementById("textarea6").style.display != 'none') {
+        document.getElementById("textarea6").style.display = 'none';
+        document.getElementById("seeMore6").innerHTML = 'Description';
+        document.getElementById("seeMore6").style.color = "blue";        
+    }
+}
+
+function toggleSeeMore7() {
+    if(document.getElementById("textarea7").style.display == 'none') {
+        document.getElementById("textarea7").style.display = 'block';
+        document.getElementById("seeMore7").innerHTML = 'See less';
+        document.getElementById("seeMore7").style.color = "red";
+    }
+    else if (document.getElementById("textarea7").style.display != 'none') {
+        document.getElementById("textarea7").style.display = 'none';
+        document.getElementById("seeMore7").innerHTML = 'Description';
+        document.getElementById("seeMore7").style.color = "blue";        
+    }
+}
